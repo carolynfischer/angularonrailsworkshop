69561cb37b8e7f9c06682a6364cc212ef7349ab1
Carolyn Fischer <carolyn.fischer7@gmail.com>
2014-12-13 13:10:12 -0800

    answers controller has create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,4 +2,10 @@ class AnswersController < ApplicationController
   def index
     render json: Answer.all
   end
+
+  def create
+    params
+    answer = Answer.create email: params[:email], body: params[:body], question_id: params[:question_id]
+    render json: answer
+  end
 end
