4e8c80025212b3971c3938bf8670253549167d96
Carolyn Fischer <carolyn.fischer7@gmail.com>
2014-12-13 14:58:05 -0800

    submitting works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,36 @@
 //= require_tree .
 
 console.log("Works!")
+
+var fakeData = [
+	{ email: "carolyna@carolyn.ee", body: "A question by A" },
+	{ email: "carolynb@carolyn.ee", body: "A question by B" },
+	{ email: "carolync@carolyn.ee", body: "A question by C" },
+	{ email: "carolynd@carolyn.ee", body: "A question by D" }
+];
+
 var AoRApp = angular.module('AoR', [])
-.controller('MainController', function($scope) {
+.controller('MainController', 
+function($scope, $http) {
   $scope.var1 = 'CodingHouse';
+  // $scope.questions = fakeData;
+  $http.get("/questions").then(function(response){
+    debugger;
+    $scope.questions = response.data;
+  });
+
+  $scope.addQuestion = function() {
+    debugger;
+    $http.post("/questions", {
+	email: $scope.email,
+        body: $scope.body
+    }).success(function(data) {
+        console.log(data);
+        $scope.questions.push(data);
+        $("#askQuestionModel").hide();
+    }).error(function(error) {
+        console.log(error);
+    })
+  } 
 });
 
